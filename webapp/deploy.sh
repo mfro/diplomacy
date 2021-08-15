38663d528ffc78c147faa5e76e67853fcf5a3b2b
Max Froehlich <max@mfro.me>
2021-08-15 15:56:36 -0400

    Fix deploy script

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,3 +1,19 @@
+if [ ! -d "gh-pages" ]; then
+  origin=$(git remote get-url origin)
+  git clone --branch "gh-pages" "$origin" "gh-pages"
+  code=0
+  if [ $code != 0 ]; then
+    mkdir "gh-pages"
+    cd "gh-pages"
+    git init
+    git remote add origin "$origin"
+    git checkout --orphan "gh-pages"
+    git commit --allow-empty --message "create gh-pages"
+    git push -u origin "gh-pages"
+    cd ..
+  fi
+fi
+
 yarn build
 
 cd "gh-pages"
